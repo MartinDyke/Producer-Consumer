ec3b2edc2305ea79ac39c479bd05ee4e823c5a3e
Martin Dyke <mdyke91@gmail.com>
2018-06-11 21:50:25 +0100

    side by side updates

--- ProducerConsumer.py
+++ ProducerConsumer.py
@@ -1,15 +1,9 @@
-import statistics
 import random
+import time
 import timeit
-import multiprocessing
 from threading import Thread
 
-
-# What defines what a quote is?
-# - The logic of getRandomQuote() is fine but it should return a concrete object of type Quote.
-# - There is nothing stopping somebody building on top of your Producer class and changing for example, the order of the
-#   quote items eg: [random.randint(1, 100), self.companies[random.randint(0, len(self.companies) - 1)]]. With a defined
-#   quote class you are defining and enforcing what it means to be a quote to everybody else.
+# Avg runtime: 1.572
 
 class Quote:
     def __init__(self, companies):
@@ -34,9 +28,6 @@ class Buffer:
     def __init__(self):
         self.quotes = []  # quotes to be stored in list in format [['GOOG',15],['AMAZ',20],...]
 
-    # Why do your add and remove methods guarantee that the quotes will be consumed in the same order that they were
-    # produced? The impl is fine (for now) but why does it work? What are append and remove doing underneath?
-
     def add(self, quote):
         self.quotes.append(quote)
 
@@ -56,14 +47,15 @@ class Consumer:
                        companies}  # [counter, min, average,max]
         self.companies = companies
         self.printer = Printer(file)
+        
     def reset(self):
         self.values= {i: [0,100,0,0] for i in self.companies}
 
     def run(self):
         Day=1
-        while Day!=101:
-            quoteNo = 1
-            while quoteNo != 1000:
+        while Day!=11:  # for 100 days do:
+            quoteNo = 0
+            while quoteNo != 2500:  # while quoteNo is less than 1000 do:
                 if not self.buffer.isEmpty():
                     a = self.buffer.remove()
                     self.values[a.company][0] +=1
@@ -71,12 +63,23 @@ class Consumer:
                     if a.value < self.values[a.company][1]: self.values[a.company][1]=a.value
                     if a.value > self.values[a.company][3]: self.values[a.company][3]=a.value
                     quoteNo += 1
-            self.printer.printToFile('Day ' + str(Day) + ":\n\n")
-            for key in self.values:
-                if self.values[key][0] !=0:
-                    self.printer.printToFile('The highest value for ' + key + ' is ' + str(self.values[key][3]) + "\n" +
-                                             'The lowest value for ' + key + ' is ' + str(self.values[key][1]) + "\n" +
-                                             'The average value for ' + key + ' is ' + str("{0:.1f}".format(self.values[key][2]/self.values[key][0])) + '\n\n\n' )
+            # self.printer.printToFile('Day ' + str(Day) + "\n")
+            # for key in self.values:
+            #      if self.values[key][0] !=0:
+            #          self.printer.printToFile('The highest value for ' + key + ' is ' + str(self.values[key][3]) + "\n" +
+            #                                   'The lowest value for ' + key + ' is ' + str(self.values[key][1]) + "\n" +
+            #                                   'The average value for ' + key + ' is ' + str("{0:.1f}".format(self.values[key][2]/self.values[key][0])) + '\n' )
+            #
+            # Print to console:
+            # print('Day ' + str(Day) + ":\n")
+            # for key in self.values:
+            #      if self.values[key][0] != 0:
+            #          print('The highest value for ' + key + ' is ' + str(self.values[key][3]) + "\n" +
+            #                                   'The lowest value for ' + key + ' is ' + str(self.values[key][1]) + "\n" +
+            #                                   'The average value for ' + key + ' is ' + str(
+            #              "{0:.1f}".format(self.values[key][2] / self.values[key][0])) + '\n')
+            
+            time.sleep(0.001)
             Day+=1
             self.reset()
         
@@ -85,21 +88,35 @@ class Printer:
     def __init__(self,file):
         self.file = file
     
-    def printToFile(self,text):
+    def printToFile(self, text):
         open(self.file,"a").write(text)
     
 
 if __name__ == "__main__":
-    T1 = timeit.default_timer()
     b = Buffer()
     comp = ['AAPL', 'AMZN', 'GOOG', 'FB', 'CSCO', 'CMCSA', 'AMGN', 'ADBE', 'GILD', 'COST']
     p = Producer(b, comp)
-    c = Consumer(b, comp,"Output.txt")
+    c1 = Consumer(b, comp, "Output1.txt")
+    c2 = Consumer(b, comp, "Output2.txt")
+    c3 = Consumer(b, comp, "Output3.txt")
+    c4 = Consumer(b, comp, "Output4.txt")
     t1 = Thread(target=p.run)
-    t2 = Thread(target=c.run)
+    t2 = Thread(target=c1.run)
+    t3 = Thread(target=c2.run)
+    t4 = Thread(target=c3.run)
+    t5 = Thread(target=c4.run)
+
+    T1 = timeit.default_timer()
     t1.start()
+    time.sleep(0.01)
     t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
     t1.join()
     t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
     T2 = timeit.default_timer()
     print("Total elapsed time is " + str(T2 - T1))
